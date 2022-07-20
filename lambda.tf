8ed7adcebefd909372cabc047cc136d464a0aee5
Surya Vara Prasad Edupuganti <surya_edupuganti@persistent.com>
2022-07-21 02:16:49 +0530

    changes in lambda file

--- lambda.tf
+++ lambda.tf
@@ -13,7 +13,7 @@ data "archive_file" "welcome" {
 resource "aws_lambda_function" "test_lambda" {
   filename      = "${local.lambda_zip_location}"
   function_name = "welcome"
-  role          = "${aws_iam_role.welcome.arn}"
+  role          = "${aws_iam_role_policy.lambda_role.arn}"
   handler       = "welcome.hello"
 
   runtime = "python3.9"
